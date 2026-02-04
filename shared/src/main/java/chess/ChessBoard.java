5e31de48798ee4efea3a684f753b3a2921def988
Cameron Kersey <cadeke@byu.edu>
2026-02-03 17:02:47 -0800

    Update ChessBoard.java

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,9 +7,11 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private ChessPiece[][] myboard;
 
     public ChessBoard() {
-        
+        myboard = new ChessPiece[8][8];
+        //resetBoard();
     }
 
     /**
@@ -19,7 +21,16 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        int r=calcRow(position); int c=calcCol(position);
+        myboard[r][c]=piece;
+        //throw new RuntimeException("Not implemented");
+    }
+
+    public int calcRow(ChessPosition pos){
+        return pos.getRow()-1;
+    }
+    public int calcCol(ChessPosition pos){
+        return pos.getColumn()-1;
     }
 
     /**
@@ -30,7 +41,9 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        int r=calcRow(position); int c=calcCol(position);
+        return myboard[r][c];
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -38,6 +51,59 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        ChessPiece.PieceType[] backrow = {
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK
+        };
+
+        for (int a = 0; a < 8; a++) {
+            for (int b = 0; b < 8; b++) {
+                myboard[a][b] = null;
+            }
+        }
+
+        for (int b = 1; b <= 8; b++) {
+            addPiece(new ChessPosition(2, b), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7, b), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(1, b), new ChessPiece(ChessGame.TeamColor.WHITE, backrow[b-1]));
+            addPiece(new ChessPosition(8, b), new ChessPiece(ChessGame.TeamColor.BLACK, backrow[b-1]));
+        }
+        //throw new RuntimeException("Not implemented");
     }
-}
+
+    @Override
+    public boolean equals(Object o){
+        if(this==o)return true;
+        if(o==null || getClass()!=o.getClass())return false;
+        ChessBoard no = (ChessBoard) o;
+        for(int a=0;a<8;a++){
+            for(int b=0;b<8;b++){
+                ChessPiece one = this.myboard[a][b]; ChessPiece two = no.myboard[a][b];
+                if(one==null && two==null)continue;
+                if(one==null || two==null)return false;
+                if(!one.equals(two))return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode(){
+        int ans=1;
+        for(int a=0;a<8;a++){
+            for(int b=0;b<8;b++){
+                ChessPiece p = myboard[a][b];
+                int val=0;
+                if(p!=null)val=p.hashCode();
+                ans=31*ans+val;
+            }
+        }
+        return ans;
+    }
+    }
